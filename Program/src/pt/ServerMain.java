16a64f10f14bc496b1e602b4c87efcc02b1a4f6c
Rodrigo Cavadas Mendes <rodrigocm1999@gmail.com>
2020-10-25 21:51:50 +0000

    hgireuhger

---
+++ Program/src/pt/ServerMain.java
@@ -0,0 +1,21 @@
+package pt;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class ServerMain {
+
+	public static final int serverPort = 9321;
+	
+	public static void main(String[] args) {
+		
+		/*Class.forName("com.mysql.jdbc.Driver").newInstance();
+		Connection con = DriverManager.getConnection(
+				"jdbc:mysql://rodrigohost.ddns.net:3306/main","server","VeryStrongPassword");
+		Statement stmt = con.createStatement();*/
+		
+		
+	}
+	
+}
