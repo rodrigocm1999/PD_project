fb249c6db877c7b6a7103d9f392b74407aad61ff
Zigko <cenas.dorin2@hotmail.com>
2020-10-26 18:00:45 +0000

    asd

--- Program/src/pt/UserInfo.java
+++ Program/src/pt/UserInfo.java
@@ -7,4 +7,12 @@ public class UserInfo implements Serializable {
 	public String username;
 	public String password;
 	public String photoPath;
+
+
+	public UserInfo(String name, String username, String password, String photoPath) {
+		this.name = name;
+		this.username = username;
+		this.password = password;
+		this.photoPath = photoPath;
+	}
 }
