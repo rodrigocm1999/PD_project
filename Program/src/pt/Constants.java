da7678566c00f0ac28736d795bf95ad93928eed7
Rodrigo Cavadas Mendes <rodrigocm1999@gmail.com>
2020-10-26 17:48:34 +0000

    yeqwa

--- Program/src/pt/Constants.java
+++ Program/src/pt/Constants.java
@@ -9,4 +9,10 @@ public class Constants {
 	public static final String CONNECTION_ACCEPTED = "CONNECTION_ACCEPTED";
 	public static final String CONNECTION_REFUSED = "CONNECTION_REFUSED";
 	
+	public  static final String REGISTER = "REGISTER_ATTEMPT";
+	public  static final String REGISTER_SUCCESS = "REGISTER_SUCCESS";
+	public  static final String LOGIN = "LOGIN_ATTEMPT";
+	public  static final String LOGIN_SUCCESS = "LOGIN_SUCCESS";
+
+	
 }
---
+++ Program/src/pt/UserInfo.java
@@ -0,0 +1,10 @@
+package pt;
+
+import java.io.Serializable;
+
+public class UserInfo implements Serializable {
+	public String name;
+	public String username;
+	public String password;
+	public String photoPath;
+}
