5782f03618389ce80d6c63ba389f5f8d8f8bd001
Zigko <cenas.dorin2@hotmail.com>
2020-10-30 17:50:07 +0000

    +2

--- Program/Client/src/pt/Client/ClientMain.java
+++ Program/Client/src/pt/Client/ClientMain.java
@@ -12,78 +12,79 @@ import java.net.*;
 import java.util.ArrayList;
 
 public class ClientMain {
-	
-	private InetAddress serverIPAddress;
-	private int portUDPServer;
-	private Socket socket;
-	private static ObjectOutputStream oOS;
-	private static ObjectInputStream oIS;
-	private ArrayList<ServerAddress> serversList;
-	private static ClientMain instance;
-	
-	public static ClientMain getInstance() {
-		return instance;
-	}
-	
-	public ClientMain(String ipServer, int port) throws Exception {
-		if (instance != null) {
-			throw new Exception("Client Main Exists");
-		}
-		instance = this;
-		this.serverIPAddress = InetAddress.getByName(ipServer);
-		this.portUDPServer = port;
-	}
-	
-	public void connectToServer() throws Exception {
-		DatagramSocket datagramSocket = new DatagramSocket();
-		
-		while (true) {
-			boolean success = tryConnectServer(serverIPAddress, portUDPServer, datagramSocket);
-			if (success) {
-				return;
-			} else {
-				ServerAddress serverAddress = serversList.get(0);
-				serverIPAddress = serverAddress.getAddress();
-				portUDPServer = serverAddress.getUDPPort();
-				//TODO test this
-			}
-		}
-	}
-	
-	private boolean tryConnectServer(InetAddress ipAddress, int port, DatagramSocket udpSocket) throws Exception {
-		Command command = new Command(Constants.ESTABLISH_CONNECTION);
-		UDPHelper.sendUDPObject(command, udpSocket, ipAddress, port);
-		
-		udpSocket.setSoTimeout(1500);
-		command = (Command) UDPHelper.receiveUDPObject(udpSocket);
-		String protocol = command.getProtocol();
-		
-		if (protocol.equals(Constants.CONNECTION_ACCEPTED)) {
-			int socketTCPort = (int) command.getExtras();
-			socket = new Socket(ipAddress, socketTCPort);
-			oOS = new ObjectOutputStream(socket.getOutputStream());
-			oIS = new ObjectInputStream(socket.getInputStream());
-			
-			command = (Command) oIS.readObject();
-			if (!command.getProtocol().equals(Constants.SERVERS_LIST)) {
-				throw new Exception("Should not happen");
-			}
-			serversList = (ArrayList<ServerAddress>) command.getExtras();
-			return true;
-		} else if (protocol.equals(Constants.CONNECTION_REFUSED)) {
-			// TODO Garantir que recebe
-			serversList = (ArrayList<ServerAddress>) UDPHelper.receiveUDPObject(udpSocket);
-			return false;
-		} else {
-			throw new IOException("Illegal Connection Protocol");
-		}
-	}
 
-	public Object sendCommandToServer(String protocol, Object object) throws IOException, ClassNotFoundException {
-		oOS.writeObject(new Command(protocol, object));
-		Object ob = oIS.readObject();
-		return ob;
-	}
+    private InetAddress serverIPAddress;
+    private int portUDPServer;
+    private Socket socket;
+    private static ObjectOutputStream oOS;
+    private static ObjectInputStream oIS;
+    private ArrayList<ServerAddress> serversList;
+    private static ClientMain instance;
 
-	
+    public static ClientMain getInstance() {
+        return instance;
+    }
+
+    public ClientMain(String ipServer, int port) throws Exception {
+        if (instance != null) {
+            throw new Exception("Client Main Exists");
+        }
+        instance = this;
+        this.serverIPAddress = InetAddress.getByName(ipServer);
+        this.portUDPServer = port;
+    }
+
+    public void connectToServer() throws Exception {
+        DatagramSocket datagramSocket = new DatagramSocket();
+
+        while (true) {
+            boolean success = tryConnectServer(serverIPAddress, portUDPServer, datagramSocket);
+            if (success) {
+                return;
+            } else {
+                ServerAddress serverAddress = serversList.get(0);
+                serverIPAddress = serverAddress.getAddress();
+                portUDPServer = serverAddress.getUDPPort();
+                //TODO test this
+            }
+        }
+    }
+
+    private boolean tryConnectServer(InetAddress ipAddress, int port, DatagramSocket udpSocket) throws Exception {
+        System.out.println("inicio a entrar ");
+        Command command = new Command(Constants.ESTABLISH_CONNECTION);
+        UDPHelper.sendUDPObject(command, udpSocket, ipAddress, port);
+
+        udpSocket.setSoTimeout(1500);
+        command = (Command) UDPHelper.receiveUDPObject(udpSocket);
+        String protocol = command.getProtocol();
+        System.out.println(command);
+        if (protocol.equals(Constants.CONNECTION_ACCEPTED)) {
+            int socketTCPort = (int) command.getExtras();
+            socket = new Socket(ipAddress, socketTCPort);
+            oOS = new ObjectOutputStream(socket.getOutputStream());
+            oIS = new ObjectInputStream(socket.getInputStream());
+
+            command = (Command) oIS.readObject();
+            if (!command.getProtocol().equals(Constants.SERVERS_LIST)) {
+                throw new Exception("Should not happen");
+            }
+            serversList = (ArrayList<ServerAddress>) command.getExtras();
+            return true;
+        } else if (protocol.equals(Constants.CONNECTION_REFUSED)) {
+            // TODO Garantir que recebe
+            serversList = (ArrayList<ServerAddress>) UDPHelper.receiveUDPObject(udpSocket);
+            return false;
+        } else {
+            throw new IOException("Illegal Connection Protocol");
+        }
+    }
+
+    public Object sendCommandToServer(String protocol, Object object) throws IOException, ClassNotFoundException {
+        oOS.writeObject(new Command(protocol, object));
+        Object ob = oIS.readObject();
+        return ob;
+    }
+
+
 }
--- Program/Client/src/pt/Client/ClientWindow.java
+++ Program/Client/src/pt/Client/ClientWindow.java
@@ -9,58 +9,58 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class ClientWindow extends Application {
-	
-	private Scene scene;
-	private Stage stage;
-	private static ClientMain client;
-	
-	private static ClientWindow instance;
-	
-	public static ClientWindow getInstance() {
-		return instance;
-	}
-	
-	
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		if (instance != null) {
-			throw new Exception("Server Already Running");
-		}
-		instance = this;
-		
-		stage = primaryStage;
-		primaryStage.setTitle("");
-		Parent root = loadParent("LoginPage.fxml");
-		scene = new Scene(root, 600, 460);
-		primaryStage.setScene(scene);
-		primaryStage.show();
-	}
-	
-	
-	public void setWindowRoot(String fileName) throws IOException {
-		scene.setRoot(loadParent(fileName));
-	}
-	
-	private Parent loadParent(String fileName) throws IOException {
-		return FXMLLoader.load(getClass().getResource(fileName));
-	}
-	
-	public static void main(String[] args) {
-		
-		if (args.length != 2) {
-			System.out.println("Invalid arguments: server_address, server_UDP_port");
-		}
-		String serverAddress = args[0];
-		int port = Integer.parseInt(args[1]);
-		
-		try {
-			client = new ClientMain(serverAddress, port);
-			client.connectToServer();
-			launch(args);
-		} catch (IOException e) {
-			System.out.println("COULD NOT ACCESS THE SERVER. TRY AGAIN LATER --> " + e.getLocalizedMessage());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+
+    private Scene scene;
+    private Stage stage;
+    private static ClientMain client;
+
+    private static ClientWindow instance;
+
+    public static ClientWindow getInstance() {
+        return instance;
+    }
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        if (instance != null) {
+            throw new Exception("Server Already Running");
+        }
+        instance = this;
+
+        stage = primaryStage;
+        primaryStage.setTitle("");
+        Parent root = loadParent("LoginPage.fxml");
+        scene = new Scene(root, 600, 460);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public void setWindowRoot(String fileName) throws IOException {
+        scene.setRoot(loadParent(fileName));
+    }
+
+    private Parent loadParent(String fileName) throws IOException {
+        return FXMLLoader.load(getClass().getResource(fileName));
+    }
+
+    public static void main(String[] args) {
+
+        if (args.length != 2) {
+            System.out.println("Invalid arguments: server_address, server_UDP_port");
+        }
+        String serverAddress = args[0];
+        int port = Integer.parseInt(args[1]);
+
+        try {
+            client = new ClientMain(serverAddress, port);
+            client.connectToServer();
+            launch(args);
+        } catch (IOException e) {
+            System.out.println("COULD NOT ACCESS THE SERVER. TRY AGAIN LATER --> " + e.getLocalizedMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
